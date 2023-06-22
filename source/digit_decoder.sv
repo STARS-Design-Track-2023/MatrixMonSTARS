09c2397cfec6dfe79b87be29b42e842126f2ff98
ECN Guest008 <ecng008@potr262lnx10.ecn.purdue.edu>
2023-06-22 13:07:59 -0400

    encoder and digit decoder

---
+++ source/digit_decoder.sv
@@ -0,0 +1,18 @@
+module digit_decoder(
+    input logic keystrobe,
+    input logic [3:0] keycode,
+    output logic isdig,
+    output logic [3:0] digitCode
+ );
+
+ always_comb begin
+    if(keystrobe && keycode >= 4'b0000 && keycode < 4'b1010) begin
+        digitCode = keycode;
+        isdig = 1;
+    end
+    else begin
+        digitCode = 4'b0000;
+        isdig = 0;
+    end
+ end
+endmodule

86a4ef5889a3ab9597f0486f0c1cf85d034dc0de
arduino-is-fun <126794890+arduino-is-fun@users.noreply.github.com>
2023-06-24 20:18:00 -0400

    updated digitDecoder
    Fixed the problem of digits not showing up in seven segment display. Added a flipflop block to digit decoder

--- source/digit_decoder.sv
+++ source/digit_decoder.sv
@@ -4,15 +4,22 @@ module digit_decoder(
     output logic isdig,
     output logic [3:0] digitCode
  );
-
- always_comb begin
-    if(keystrobe && keycode < 4'b1010) begin
-        digitCode = keycode;
-        isdig = 1;
+    logic [3:0] next_digitCode;
+    always_ff @(posedge clk, negedge nrst) begin
+        if(0 == nrst) begin
+            digitCode <= 0;
+        end
+        else begin
+            digitCode <= next_digitCode;
+        end 
+    always_comb begin
+        if(keystrobe && keycode < 4'b1010) begin
+            next_digitCode = keycode;
+            isdig = 1;
+        end
+        else begin
+            next_digitCode = digitCode;
+            isdig = 0;
+        end
     end
-    else begin
-        digitCode = 4'b0000;
-        isdig = 0;
-    end
- end
 endmodule

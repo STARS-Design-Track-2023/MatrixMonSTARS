5d0c2a31bb373ac26bed487de38ef5b06615e6e9
arduino-is-fun <126794890+arduino-is-fun@users.noreply.github.com>
2023-06-27 21:04:13 -0400

    Update register_decoder.sv
    Changed the mapping of reg_num to be a 3bit output instead of a 2bit output to agree with relevant modules.

--- source/register_decoder.sv
+++ source/register_decoder.sv
@@ -2,7 +2,7 @@ module register_decoder(
     input logic clk, nrst,
     input logic [3:0] register_button,
     output logic is_reg,
-    output logic [1:0] reg_num
+    output logic [2:0] reg_num
 );
 
 logic reg_async, reg_sync, reg_i;
@@ -34,14 +34,14 @@ end
 always_comb begin
     if(is_reg) begin
         case(register_button)
-            4'b0001: next_reg_num = 2'b00;
-            4'b0010: next_reg_num = 2'b01;
-            4'b0100: next_reg_num = 2'b10;
-            4'b1000: next_reg_num = 2'b11;
+            4'b0001: next_reg_num = 3'b001;
+            4'b0010: next_reg_num = 3'b010;
+            4'b0100: next_reg_num = 3'b011;
+            4'b1000: next_reg_num = 3'b100;
         endcase
     end
     else begin
-        next_reg_num = 2'b00;
+        next_reg_num = 3'b000;
     end
 end
 endmodule
